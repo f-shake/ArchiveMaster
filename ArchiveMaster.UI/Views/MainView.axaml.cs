7a8c442809ce4419e0ec80fb88a1080ba399134c
fshake <f-shake@outlook.com>
2025-09-12 09:46:06 +0800

    细节优化；修复打包后无法运行的BUG（Svg.Controls.Skia.Avalonia新版本问题）

--- ArchiveMaster.UI/Views/MainView.axaml.cs
+++ ArchiveMaster.UI/Views/MainView.axaml.cs
@@ -38,6 +38,8 @@ public partial class MainView : UserControl
     private readonly IDialogService dialogService;
     private readonly IPermissionService permissionService;
 
+    private bool isFirstLoad = true;
+
     public MainView(MainViewModel viewModel,
         AppConfig appConfig,
         IDialogService dialogService,
@@ -90,15 +92,6 @@ public partial class MainView : UserControl
         WeakReferenceMessenger.Default.Cleanup();
     }
 
-    // private void ToolItem_OnKeyDown(object sender, KeyEventArgs e)
-    // {
-    //     if (e.Key == Key.Enter)
-    //     {
-    //         TopLevel.GetTopLevel(this).FocusManager.ClearFocus();
-    //         (DataContext as MainViewModel).EnterToolCommand.Execute((sender as ToolItemBox).DataContext);
-    //     }
-    // }
-
     private void BeginChangingContent()
     {
         Dispatcher.UIThread.Invoke(() => IsHitTestVisible = false);
@@ -107,6 +100,76 @@ public partial class MainView : UserControl
     private void EndChangingContent()
     {
         Dispatcher.UIThread.Invoke(() => IsHitTestVisible = true);
+    }
+
+    private async void EnterNewTool(ToolPanelInfo panelInfo)
+    {
+        try
+        {
+            TimeSpan animationDuration = (TimeSpan)Resources["AnimationDuration"];
+
+            //若为空，则新建页面对象
+            if (panelInfo.PanelInstance == null)
+            {
+                panelInfo.PanelInstance = HostServices.GetService(panelInfo.ViewType) as PanelBase ??
+                                          throw new Exception($"无法找到{panelInfo.ViewType}服务");
+                panelInfo.PanelInstance.Title = panelInfo.Title;
+                panelInfo.PanelInstance.Description = panelInfo.Description;
+            }
+
+            //注册ViewModel的事件，通知进入
+            if (panelInfo.PanelInstance.DataContext is ViewModelBase vm)
+            {
+                vm.PropertyChanged += PanelViewModelPropertyChanged;
+                vm.RequestClosing += PanelViewModelRequestClosing;
+                vm.OnEnter();
+            }
+
+            mainContent.Content = panelInfo.PanelInstance;
+            if (isFirstLoad)
+            {
+                //第一次加载特别卡，所以先显示出来，然后等一会儿再开始动画
+                await Task.Delay(animationDuration);
+                isFirstLoad = false;
+            }
+
+            //开始进入动画
+            mainContent.Opacity = 1;
+            mainContent.RenderTransform = TransformOperations.Parse("Scale(1)");
+            await Task.Delay(animationDuration);
+            EndChangingContent();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "打开面板失败");
+            await dialogService.ShowErrorDialogAsync("打开面板失败", ex.Message);
+        }
+    }
+
+    private async Task<bool> ExitCurrentPanel()
+    {
+        if (mainContent.Content is PanelBase { DataContext: ViewModelBase vm })
+        {
+            var args = new CancelEventArgs();
+            await vm.OnExitAsync(args);
+            if (args.Cancel)
+            {
+                return true;
+            }
+
+            vm.PropertyChanged -= PanelViewModelPropertyChanged;
+            vm.RequestClosing -= PanelViewModelRequestClosing;
+
+            //开始退出动画
+            mainContent.RenderTransform = TransformOperations.Parse("Scale(0.98)");
+            mainContent.Opacity = 0.2;
+        }
+        else
+        {
+            mainContent.Opacity = 0;
+        }
+
+        return false;
     }
 
     private void PanelViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -143,25 +206,9 @@ public partial class MainView : UserControl
         }
 
         //通知已打开的面板退出
-        if (mainContent.Content is PanelBase { DataContext: ViewModelBase vm })
+        if (await ExitCurrentPanel())
         {
-            var args = new CancelEventArgs();
-            await vm.OnExitAsync(args);
-            if (args.Cancel)
-            {
-                return;
-            }
-
-            vm.PropertyChanged -= PanelViewModelPropertyChanged;
-            vm.RequestClosing -= PanelViewModelRequestClosing;
-
-            //开始退出动画
-            
-            // mainContent.RenderTransform = TransformOperations.Parse($"translate(0, {Bounds.Height}px)");
-            mainContent.RenderTransform = TransformOperations.Parse("Scale(0.98)");
-            mainContent.Opacity = 0.2;
-            // mainContent.Classes.Remove("NoBlur");
-            // mainContent.Classes.Add("Blur");
+            return;
         }
 
         //清除其他ListBox的选中项
@@ -175,37 +222,10 @@ public partial class MainView : UserControl
 
         TimeSpan animationDuration = (TimeSpan)Resources["AnimationDuration"];
         BeginChangingContent();
+        //动画期间，不做任何操作
         await Task.Delay(animationDuration);
 
         //避免页面的创建卡住UI，先让ListBox的选择响应起来
-        Dispatcher.UIThread.Post(() =>
-        {
-            //若为空，则新建页面对象
-            if (panelInfo.PanelInstance == null)
-            {
-                panelInfo.PanelInstance = HostServices.GetService(panelInfo.ViewType) as PanelBase ??
-                                          throw new Exception($"无法找到{panelInfo.ViewType}服务");
-
-                panelInfo.PanelInstance.Title = panelInfo.Title;
-                panelInfo.PanelInstance.Description = panelInfo.Description;
-            }
-
-            //注册ViewModel的事件，通知进入
-            if (panelInfo.PanelInstance.DataContext is ViewModelBase vm)
-            {
-                vm.PropertyChanged += PanelViewModelPropertyChanged;
-                vm.RequestClosing += PanelViewModelRequestClosing;
-                vm.OnEnter();
-            }
-
-            //开始进入动画
-            mainContent.Content = panelInfo.PanelInstance;
-            mainContent.Opacity = 1;
-            // mainContent.RenderTransform = TransformOperations.Parse("translate(0, 0)");
-            mainContent.RenderTransform = TransformOperations.Parse("Scale(1)");
-            // mainContent.Classes.Remove("Blur");
-            // mainContent.Classes.Add("NoBlur");
-            _ = Task.Delay(animationDuration).ContinueWith(t => EndChangingContent());
-        });
+        Dispatcher.UIThread.Post(() => EnterNewTool(panelInfo));
     }
 }
