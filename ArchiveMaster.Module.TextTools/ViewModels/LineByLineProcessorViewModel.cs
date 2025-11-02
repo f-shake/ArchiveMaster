baa5920dca4d5e8c02800650c2408c73cb8933f1
f-shake <autodotua@outlook.com>
2025-11-02 14:42:08 +0800

    逐行处理新增从文本导入示例表格

--- ArchiveMaster.Module.TextTools/ViewModels/LineByLineProcessorViewModel.cs
+++ ArchiveMaster.Module.TextTools/ViewModels/LineByLineProcessorViewModel.cs
@@ -12,6 +12,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using ArchiveMaster.ViewModels.FileSystem;
 using FzLib.Avalonia.Dialogs;
+using FzLib.Text;
 using Serilog;
 
 namespace ArchiveMaster.ViewModels;
@@ -44,6 +45,44 @@ public partial class LineByLineProcessorViewModel(ViewModelServices services)
         var item = new LineByLineItem();
         Config.Examples.Add(item);
         SelectedExample = item;
+    }
+
+    [RelayCommand]
+    private async Task ImportExampleFromTextAsync()
+    {
+        var text = await Services.Dialog.ShowInputMultiLinesTextDialogAsync("从文本导入",
+            "在此处粘贴表格文本，第一列为输入，第二列为输出，第三列（可选）为解释说明。列之前使用空格制表符或空格分隔。");
+        var lines = text.SplitLines();
+        Config.Examples.Clear();
+        try
+        {
+            foreach (var line in lines)
+            {
+                var parts = line.Split('\t', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 1)
+                {
+                    parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                }
+
+                if (parts.Length is not (2 or 3))
+                {
+                    throw new Exception("列的数量应当为2或3");
+                }
+
+                LineByLineItem item = new LineByLineItem();
+                item.Input = parts[0];
+                item.Output = parts[1];
+                if (parts.Length == 3)
+                {
+                    item.Explain = parts[2];
+                }
+                Config.Examples.Add(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            await Services.Dialog.ShowErrorDialogAsync("导入失败", ex);
+        }
     }
 
     [RelayCommand]
