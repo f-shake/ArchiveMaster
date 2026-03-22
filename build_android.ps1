d06e6c6b5a8e9e50e113dfd31ad4d4122057f5f0
f-shake <autodotua@outlook.com>
2026-03-22 09:08:33 +0800

    修复Android构建脚本（但apk仍然无法在部分手机上运行）

---
+++ build_android.ps1
@@ -0,0 +1,3 @@
+﻿dotnet publish ArchiveMaster.UI.Android -f net10.0-android -r android-arm64 -c Release -p:AndroidKeyStore=true  
+Write-Host "请前往输出目录\Release\ArchiveMaster.UI.Android\net*-android\android-arm64\publish\ 寻找*_Signed.apk文件"
+pause
