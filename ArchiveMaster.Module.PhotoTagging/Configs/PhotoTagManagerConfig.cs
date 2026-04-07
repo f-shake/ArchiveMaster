99354bdec1cd6948d24c5f0c742fdaade62708e9
f-shake <autodotua@outlook.com>
2026-04-07 20:50:12 +0800

    Create PhotoTagManagerConfig.cs

---
+++ ArchiveMaster.Module.PhotoTagging/Configs/PhotoTagManagerConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ArchiveMaster.Helpers;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FzLib.IO;
+
+namespace ArchiveMaster.Configs
+{
+    public partial class PhotoTagManagerConfig : ConfigBase
+    {
+        [ObservableProperty]
+        private string tagFile;
+
+        [ObservableProperty]
+        private string rootDir;
+
+        public override void Check()
+        {
+            CheckFile(TagFile, "标签文件");
+            CheckDir(RootDir, "根目录");
+        }
+    }
+}
