2dacc4a14e2f691740737b0af3ba00f20e7608ce
f-shake <autodotua@outlook.com>
2026-04-19 09:10:53 +0800

    修复了FileDataGridRowDetailDefaultContent数据上下文绑定问题

--- ArchiveMaster.Core/Views/DataGrids/FileDataGridRowDetailDefaultContent.cs
+++ ArchiveMaster.Core/Views/DataGrids/FileDataGridRowDetailDefaultContent.cs
@@ -4,12 +4,4 @@ namespace ArchiveMaster.Views;
 
 public class FileDataGridRowDetailDefaultContent : ContentControl
 {
-    protected override void OnDataContextChanged(EventArgs e)
-    {
-        base.OnDataContextChanged(e);
-        if (Content == null)
-        {
-            Content = DataContext;
-        }
-    }
 }
